5070af4b37c955aac912b2c8cb8df50a672e4866
Yuri966 <yuricm089@gmail.com>
2026-03-23 22:35:53 -0300

    feat: adicionado arquivos para saida

--- main.py
+++ main.py
@@ -12,25 +12,28 @@ def executar_projeto():
 
     print("=== Iniciando Simulador Mic-1 (UFPB) ===")
 
+    # Cria a pasta de saídas se não existir
+    pasta_saidas = os.path.join(base_dir, 'saidas')
+    os.makedirs(pasta_saidas, exist_ok=True)
+
     # --- EXECUÇÃO ETAPA 1 ---
     print("\n[Executando Etapa 1]...")
     e1_in = os.path.join(base_dir, 'Etapa_1', 'programa_etapa1.txt')
-    e1_out = os.path.join(base_dir, 'Etapa_1', 'log_etapa1.txt')
+    e1_out = os.path.join(pasta_saidas, 'log_etapa1.txt')
     try:
         rodar_etapa1(e1_in, e1_out)
-        print(f"OK: Log gerado em Etapa_1/log_etapa1.txt")
+        print(f"OK: Log gerado em saidas/log_etapa1.txt")
     except Exception as e:
         print(f"Erro na Etapa 1: {e}")
 
     # --- EXECUÇÃO ETAPA 2.1 ---
     print("\n[Executando Etapa 2.1 - ULA]...")
-    print("\n[Executando Etapa 2.1 - ULA]...")
     e21_in = os.path.join(base_dir, 'Etapa_2', 'programa_etapa2_tarefa1.txt')
-    e21_out = os.path.join(base_dir, 'Etapa_2', 'log_etapa2_tarefa1.txt')
+    e21_out = os.path.join(pasta_saidas, 'log_etapa2_tarefa1.txt')
 
     try:
         rodar_tarefa1(e21_in, e21_out) 
-        print(f"OK: Log gerado em Etapa_2/log_etapa2_tarefa1.txt")
+        print(f"OK: Log gerado em saidas/log_etapa2_tarefa1.txt")
     except Exception as e:
         print(f"Erro na Etapa 2.1: {e}")
 
@@ -38,12 +41,12 @@ def executar_projeto():
     print("\n[Executando Etapa 2.2 - Datapath]...")
     e2_prog = os.path.join(base_dir, 'Etapa_2', 'programa_etapa2_tarefa2.txt')
     e2_regs = os.path.join(base_dir, 'Etapa_2', 'registradores_etapa2_tarefa2.txt')
-    e2_log = os.path.join(base_dir, 'Etapa_2', 'log_etapa2_tarefa2.txt')
+    e2_log = os.path.join(pasta_saidas, 'log_etapa2_tarefa2.txt')
     
     try:
         maquina = Mic1Datapath()
         maquina.executar(e2_prog, e2_regs, e2_log)
-        print(f"OK: Log gerado em Etapa_2/log_etapa2_tarefa2.txt")
+        print(f"OK: Log gerado em saidas/log_etapa2_tarefa2.txt")
     except Exception as e:
         print(f"Erro na Etapa 2.2: {e}")
 
